1179ec6c240732ad6a8ee548c41092ab4338d6ac
Linda Okorie <lindaokorie27@gmail.com>
2020-06-10 13:44:48 +0100

    responsiveness worked on

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,7 +3,9 @@ const navbar = document.querySelector('.navbar-custom');
 window.addEventListener('scroll', () => {
     if(window.scrollY == 0) {
         navbar.style.backgroundColor = '';
+        navbar.style.borderBottomColor = "#ffffff";
     } else {
         navbar.style.backgroundColor = '#3B8CC6';
+        navbar.style.borderBottomColor = "#3B8CC6";
     }
 });
